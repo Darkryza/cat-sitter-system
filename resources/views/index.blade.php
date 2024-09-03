42dc2a5b8151d5596b9a32066c0baa15a60a2ec1
Darkryza <darkryza123@gmail.com>
2024-09-03 14:50:05 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,16 +9,18 @@
             <li><a href="#">More</a></li>
         </ul>
     </nav>
-    <div class="container">
-        <div class="hero1">
-            <img src="{{ asset('images/bg1.jpg') }}" alt="bg" class="bg">
+    <div class="hero">
+        <img src="{{ asset('images/bg1.jpg') }}" alt="bg" class="bg">
+        <div class="welcome-title">
             <h1 class="title">WELCOME TO <br> CAT SITTER</h1>
         </div>
-        <div class="hero2">
-            <img src="{{ asset('images/bg2.jpg') }}" alt="" class="bg">
-            <h2 class="about-us">ABOUT US</h2>
-            <img src="{{ asset('images/cat.png') }}" alt="cat" class="cat">
-            <div class="text-about-us">
+    </div>
+    <div class="hero">
+        <img src="{{ asset('images/bg2.jpg') }}" alt="" class="bg">
+        <img src="{{ asset('images/cat.png') }}" alt="cat" class="cat">
+        <div class="about-us-container">
+            <h2 class="about-us-title">ABOUT US</h2>
+            <div class="about-us-text">
                 
             </div>
         </div>
