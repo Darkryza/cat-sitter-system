ce6b392a4c92fb07174dc6089477e83c8684934c
Darkryza <darkryza123@gmail.com>
2024-09-03 20:44:35 +0800

    update

--- resources/views/include/layout.blade.php
+++ resources/views/include/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>@yield('title','Cat Sitter')</title>
 </head>
 <body>
